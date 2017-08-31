af1d28bc8fb731157c602cabc8a99ef34cbbc8bb
Andreas Prudzilko <mendrik76@gmail.com>
2017-08-31 20:22:23 +0300

    add support for bequeath

--- src/com/feather/idea/FeatherUtil.java
+++ src/com/feather/idea/FeatherUtil.java
@@ -8,21 +8,16 @@ import com.intellij.lang.javascript.psi.ecma6.TypeScriptField;
 import com.intellij.lang.javascript.psi.ecma6.TypeScriptFunction;
 import com.intellij.lang.javascript.psi.ecmal4.JSQualifiedNamedElement;
 import com.intellij.lang.javascript.psi.resolve.JSResolveUtil;
-import com.intellij.lang.javascript.psi.stubs.JSClassIndex;
+import com.intellij.lang.javascript.psi.stubs.JSSymbolIndex2;
 import com.intellij.openapi.project.Project;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.search.GlobalSearchScope;
 import com.intellij.psi.stubs.StubIndex;
-import com.intellij.psi.stubs.StubIndexKey;
 import com.intellij.psi.util.PsiTreeUtil;
-import java.util.Collection;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.stream.Stream;
 
 class FeatherUtil {
-
-    public static final StubIndexKey<String, TypeScriptClass> KEY = StubIndexKey.createIndexKey("typescript.class.index");
 
     static Optional<JSQualifiedNamedElement> findField(String property, PsiElement element) {
         TypeScriptClass parent = PsiTreeUtil.getContextOfType(element, TypeScriptClass.class);
@@ -37,38 +32,20 @@ class FeatherUtil {
                     .findFirst();
             if (field.isPresent()) {
                 return field;
-            } else {
-                return findBequeathProperty(property, parent);
             }
         }
-        return Optional.empty();
+        return findBequeathProperty(property, parent);
     }
 
-    private static Optional<JSQualifiedNamedElement> findBequeathProperty(String property, PsiElement classElement) {
-        PsiElement context = classElement.getContext();
-        if (context != null) {
-            Project project = classElement.getProject();
-            GlobalSearchScope scope = GlobalSearchScope.allScope(project); // JSResolveUtil.getResolveScope(context);
-            Collection<JSElement> elements = StubIndex.getElements(JSClassIndex.KEY, property, project, scope, JSElement.class);
-            if (elements != null) {
-                return elements.stream()
-                        .flatMap(e -> findPropertyInClass(e, property))
-                        .findFirst();
-            }
-
-        }
-        return Optional.empty();
-    }
-
-    private static Stream<JSQualifiedNamedElement> findPropertyInClass(JSElement e, String property) {
-        JSQualifiedNamedElement[] children = PsiTreeUtil.getChildrenOfType(e, JSQualifiedNamedElement.class);
-        if (children != null) {
-            Optional<JSQualifiedNamedElement> first = Stream.of(children)
-                    .filter(f -> f.getName().equals(property) && isBequeathProperty(f))
-                    .findFirst();
-            return first.map(Stream::of).orElseGet(Stream::empty);
-        }
-        return Stream.empty();
+    private static Optional<JSQualifiedNamedElement> findBequeathProperty(String property, JSElement classElement) {
+        Project project = classElement.getProject();
+        GlobalSearchScope scope = JSResolveUtil.getResolveScope(classElement);
+        return StubIndex.getElements(JSSymbolIndex2.KEY, property, project, scope, JSElement.class)
+                .stream()
+                .filter(e -> e instanceof JSQualifiedNamedElement)
+                .map(e -> ((JSQualifiedNamedElement) e))
+                .filter(FeatherUtil::isBequeathProperty)
+                .findFirst();
     }
 
     private static boolean isBequeathProperty(JSQualifiedNamedElement element) {
